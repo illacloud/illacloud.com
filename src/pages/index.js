e9c7130cb0b1c65462b511c8feb44b6370d78015
smallSohoSolo <b3069741@gmail.com>
2022-05-20 17:16:03 +0800

    feat: change style

--- src/pages/index.js
+++ src/pages/index.js
@@ -72,7 +72,7 @@ function Header() {
             </div>
           </div>
         </div>
-        <div className="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
+        <div className="relative max-w-5xl mx-auto pt-20 pb-20 sm:pt-24 lg:pt-32">
           <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
             Help developers build Business Tools more efficiently.
           </h1>
