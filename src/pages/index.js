e481a3bc0a28b53f6af9dc22923da682a5f64539
xiaoyu <lavamint@outlook.com>
2022-08-26 18:38:25 +0800

    feat: update desc

--- src/pages/index.js
+++ src/pages/index.js
@@ -75,7 +75,7 @@ const Home = () => {
           content="ILLA Cloud - Accelerate your internal tools development."
         />
         <title>{t("meta.title")}</title>
-        {/*<meta name="description" content={t("meta.description")} />*/}
+        <meta name="description" content={t("meta.description")} />
         <meta name="keywords" content="illa,illacloud,illa cloud,艾拉云,艾拉云科,艾拉" />
       </Head>
       <div className={"bg-[#ffffff] sm:bg-[#fafafa] "}>
