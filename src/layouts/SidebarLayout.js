f48f5dd855e933b77af5c11ee7bc948167dfe5f1
xiaoyu <lavamint@outlook.com>
2022-09-06 10:48:15 +0800

    feat: update

--- src/layouts/SidebarLayout.js
+++ src/layouts/SidebarLayout.js
@@ -99,44 +99,44 @@ function Nav({ nav, children, fallbackHref, mobile = false }) {
     <nav ref={scrollRef} id="nav" className="lg:text-sm lg:leading-6 relative">
       <div className="sticky top-0 -ml-0.5 pointer-events-none">
         {!mobile && <div className="h-10 bg-white dark:bg-slate-900" />}
-        <div className="bg-white dark:bg-slate-900 relative pointer-events-auto">
-          <SearchButton className="hidden w-full lg:flex items-center text-sm leading-6 text-slate-400 rounded-md ring-1 ring-slate-900/10 shadow-sm py-1.5 pl-2 pr-3 hover:ring-slate-300 dark:bg-slate-800 dark:highlight-white/5 dark:hover:bg-slate-700">
-            {({ actionKey }) => (
-              <>
-                <svg
-                  width="24"
-                  height="24"
-                  fill="none"
-                  aria-hidden="true"
-                  className="mr-3 flex-none"
-                >
-                  <path
-                    d="m19 19-3.5-3.5"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <circle
-                    cx="11"
-                    cy="11"
-                    r="6"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-                Quick search...
-                {actionKey && (
-                  <span className="ml-auto pl-3 flex-none text-xs font-semibold">
-                    {actionKey[0]}K
-                  </span>
-                )}
-              </>
-            )}
-          </SearchButton>
-        </div>
+        {/*<div className="bg-white dark:bg-slate-900 relative pointer-events-auto">*/}
+        {/*  <SearchButton className="hidden w-full lg:flex items-center text-sm leading-6 text-slate-400 rounded-md ring-1 ring-slate-900/10 shadow-sm py-1.5 pl-2 pr-3 hover:ring-slate-300 dark:bg-slate-800 dark:highlight-white/5 dark:hover:bg-slate-700">*/}
+        {/*    {({ actionKey }) => (*/}
+        {/*      <>*/}
+        {/*        <svg*/}
+        {/*          width="24"*/}
+        {/*          height="24"*/}
+        {/*          fill="none"*/}
+        {/*          aria-hidden="true"*/}
+        {/*          className="mr-3 flex-none"*/}
+        {/*        >*/}
+        {/*          <path*/}
+        {/*            d="m19 19-3.5-3.5"*/}
+        {/*            stroke="currentColor"*/}
+        {/*            strokeWidth="2"*/}
+        {/*            strokeLinecap="round"*/}
+        {/*            strokeLinejoin="round"*/}
+        {/*          />*/}
+        {/*          <circle*/}
+        {/*            cx="11"*/}
+        {/*            cy="11"*/}
+        {/*            r="6"*/}
+        {/*            stroke="currentColor"*/}
+        {/*            strokeWidth="2"*/}
+        {/*            strokeLinecap="round"*/}
+        {/*            strokeLinejoin="round"*/}
+        {/*          />*/}
+        {/*        </svg>*/}
+        {/*        Quick search...*/}
+        {/*        {actionKey && (*/}
+        {/*          <span className="ml-auto pl-3 flex-none text-xs font-semibold">*/}
+        {/*            {actionKey[0]}K*/}
+        {/*          </span>*/}
+        {/*        )}*/}
+        {/*      </>*/}
+        {/*    )}*/}
+        {/*  </SearchButton>*/}
+        {/*</div>*/}
         {!mobile && <div className="h-8 bg-gradient-to-b from-white dark:from-slate-900" />}
       </div>
       <ul>
