03774b956732ec5d55396114ecf9f630cfef2864
Aruseito <41471697+AruSeito@users.noreply.github.com>
2022-10-11 17:43:24 +0800

    fix: global style

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -68,7 +68,7 @@ export default class Document extends NextDocument {
         </Head>
         <body
           className={clsx('antialiased text-slate-500 dark:text-slate-400', {
-            'bg-[#fafafa] dark:bg-slate-900':
+            'bg-white dark:bg-slate-900':
               !this.props.dangerousAsPath.startsWith('/examples/'),
           })}
         >
