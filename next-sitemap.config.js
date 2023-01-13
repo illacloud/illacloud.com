1feec9829601b1b09de61cc1ea77aa304545c496
smallSohoSolo <b3069741@gmail.com>
2023-01-13 16:52:24 +0800

    fix: sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,11 +2,4 @@
 module.exports = {
   siteUrl: 'https://www.illacloud.com',
   generateRobotsTxt: true,
-  // optional
-  robotsTxtOptions: {
-    additionalSitemaps: [
-      'https://www.illacloud.com/docs/about-illa.xml',
-      'https://www.illacloud.com/docs/github.xml',
-    ],
-  },
 }
