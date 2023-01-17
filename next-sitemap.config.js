e460e254448caaed8bb2d6a1cc5db42e3e103da0
AruSeito <weichen@illasoft.com>
2023-01-17 16:39:11 +0800

    Revert "fix: sitemap"
    This reverts commit 1feec9829601b1b09de61cc1ea77aa304545c496.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,11 @@
 module.exports = {
   siteUrl: 'https://www.illacloud.com',
   generateRobotsTxt: true,
+  // optional
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://www.illacloud.com/docs/about-illa.xml',
+      'https://www.illacloud.com/docs/github.xml',
+    ],
+  },
 }
