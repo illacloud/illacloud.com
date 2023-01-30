3fb2d7a777569ab86acdb66341c693e743439caa
shandamengcheng <zhumengcheng@illasoft.com>
2023-01-30 16:40:07 +0800

    feat: generate sitemap file

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.illacloud.com',
+  siteUrl: process.env.SITE_URL || 'https://www.illacloud.com',
   generateRobotsTxt: true,
-  // optional
-  robotsTxtOptions: {
-    additionalSitemaps: [
-     'https://www.illacloud.com/sitemap.xml'
-    ],
-  },
 }
