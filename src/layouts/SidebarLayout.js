1ae22c51cc617e3dd4f88f039fe27799187d8f5a
shandamengcheng <zhumengcheng@illasoft.com>
2023-02-02 17:44:11 +0800

    feat: change github link to discord link

--- src/layouts/SidebarLayout.js
+++ src/layouts/SidebarLayout.js
@@ -369,7 +369,7 @@ function TopLevelNav({ mobile }) {
       </TopLevelLink>
       <TopLevelLink
         mobile={mobile}
-        href="https://github.com/illacloud/illa-builder"
+        href="https://discord.com/invite/illacloud"
         className="mb-8"
         shadow="group-hover:shadow-violet-200 dark:group-hover:bg-violet-500"
         icon={
