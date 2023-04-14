0b45b13cfde61100b51e350cc11c4da3e1b3b355
AruSeito <weichen@illasoft.com>
2023-04-14 14:35:19 +0800

    feat: ✨ linkedIn track

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -193,6 +193,25 @@ function App({ Component, pageProps, router }) {
           gtag('config', 'G-4VKRNGN7GE');
         `}
       </Script>
+      <Script strategy="afterInteractive" >
+        {` _linkedin_partner_id = "4707852";
+        window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+        window._linkedin_data_partner_ids.push(_linkedin_partner_id);`}
+      </Script>
+      <Script>
+        {`(function(l) {
+              if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q=[]}
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);})(window.lintrk);
+          `}
+      </Script>
+      <noscript>
+        <img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=4707852&fmt=gif" />
+      </noscript>
       <SearchProvider>
         {showHeader && (
           <Header
