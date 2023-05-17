7c2b39cfce1fc76659d0d7c106e8a4868574b851
wangtaofeng <1507337624@qq.com>
2023-05-17 14:49:43 +0800

    feat: update landing page componenets style

--- src/components/LandingPage/AsyncIndexContent.js
+++ src/components/LandingPage/AsyncIndexContent.js
@@ -1,9 +1,9 @@
-import { Fragment } from 'react';
+import { Fragment } from 'react'
 import { useTranslation } from 'next-i18next'
 import style from '@/components/LandingPage/index.module.css'
 import { sendTagEvent } from '@/utils/gtag'
-import { LearnMore } from '@/components/LandingPage/LearnMore';
-
+import { LearnMore } from '@/components/LandingPage/LearnMore'
+import clsx from 'clsx'
 
 const ContentItems = ({ contentList, pageName }) => {
   const { t } = useTranslation('landingPageIndex')
@@ -17,25 +17,56 @@ const ContentItems = ({ contentList, pageName }) => {
     })
   }
   if (!contentList) return null
-  return contentList.sort((a, b) => a.sort - b.sort).map(({ logo, name, description }) => {
-    return (
-      <div className={style.item} key={name}>
-        <img src={logo} alt={name} className='h-[24px] xl:h-[40px]' />
-        <div className='flex flex-col items-start gap-[4px] xl:gap-[8px]'>
-          <h3 className='xl:h-[26px] h-[20px] overflow-hidden'><span className={style.itemName}>{name}</span></h3>
-          <div className='h-[72px] overflow-hidden'><span className={style.itemDescription}>{description}</span></div>
-          <LearnMore href={`/${pageName}/${name}`} onClick={() => onClick(name)} btnText={t("learn_more")} />
+  return contentList
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ logo, name, description }) => {
+      return (
+        <div
+          className={clsx(
+            style.item,
+            pageName === 'integrations' ? style.itemBg : '',
+          )}
+          key={name}
+        >
+          {pageName === 'integrations' && (
+            <img src={logo} alt={name} className="h-[24px] xl:h-[40px]" />
+          )}
+          {pageName === 'components' && (
+            <div className={style.itemImage}>
+              <img src={logo} alt={name} className="w-full" />
+            </div>
+          )}
+          <div className="flex flex-col items-start gap-[8px]">
+            <h3 className="xl:h-[26px] h-[20px] overflow-hidden">
+              <span
+                className={clsx(
+                  style.itemNameCommon,
+                  pageName === 'integrations'
+                    ? style.itemNameIntegrations
+                    : style.itemNameComponents,
+                )}
+              >
+                {name}
+              </span>
+            </h3>
+            <div className="h-[72px] overflow-hidden">
+              <span className={style.itemDescription}>{description}</span>
+            </div>
+            <LearnMore
+              href={`/${pageName}/${name}`}
+              onClick={() => onClick(name)}
+              btnText={t('learn_more')}
+            />
+          </div>
         </div>
-      </div>
-    )
-  })
+      )
+    })
 }
-
 
 export const AsyncIndexContent = ({ content, pageName }) => {
   if (Object.keys(content).length <= 0) return null
   return (
-    <div className='flex flex-col gap-[24px] pb-[60px] xl:pb-[100px]'>
+    <div className="flex flex-col gap-[24px] pb-[60px] xl:pb-[100px]">
       {Object.values(content).map(({ title, contentList }) => {
         return (
           <Fragment key={title}>
