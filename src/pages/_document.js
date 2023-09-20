e0ccf7dfcf9158ecf5f7c296c59b1310396e61b5
Scenery <1507337624@qq.com>
2023-09-20 15:34:13 +0800

    fix: google tag build fail

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -101,7 +101,7 @@ export default class Document extends NextDocument {
               src="https://www.googletagmanager.com/ns.html?id=GTM-W52T2JWK"
               height="0"
               width="0"
-              style="display:none;visibility:hidden"
+              style={{ display: 'none', visibility: 'hidden' }}
             ></iframe>
           </noscript>
           {/* <!-- End Google Tag Manager (noscript) --> */}
