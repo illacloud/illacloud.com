2f3a79a0c1274407bc53023135b6ed9cddd3692a
Scenery <1507337624@qq.com>
2023-11-24 17:31:30 +0800

    feat: update banner bg

--- src/components/home/Content/Ways/Switch/PC/index.js
+++ src/components/home/Content/Ways/Switch/PC/index.js
@@ -20,26 +20,28 @@ const SwitchItem = ({ text, isActive, onClick }) => {
 const SwitchPC = ({ activeIndex, onClick, options }) => {
   const { t } = useTranslation('home')
   return (
-    <div className={style.switchContainerStyle}>
-      {options.map((item, i) => (
-        <Fragment key={item}>
-          <SwitchItem
-            text={t(item)}
-            isActive={i === activeIndex}
-            onClick={() => onClick(i)}
-          />
-          {i !== options.length - 1 && (
-            <div
-              className={clsx(
-                style.lineStyle,
-                activeIndex === i || activeIndex - 1 === i
-                  ? 'bg-transparent'
-                  : 'bg-white/[0.1]',
-              )}
+    <div className={style.borderStyle}>
+      <div className={style.switchContainerStyle}>
+        {options.map((item, i) => (
+          <Fragment key={item}>
+            <SwitchItem
+              text={t(item)}
+              isActive={i === activeIndex}
+              onClick={() => onClick(i)}
             />
-          )}
-        </Fragment>
-      ))}
+            {i !== options.length - 1 && (
+              <div
+                className={clsx(
+                  style.lineStyle,
+                  activeIndex === i || activeIndex - 1 === i
+                    ? 'bg-transparent'
+                    : 'bg-white/[0.1]',
+                )}
+              />
+            )}
+          </Fragment>
+        ))}
+      </div>
     </div>
   )
 }
